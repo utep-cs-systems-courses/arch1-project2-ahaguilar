0da667420fde351c0af1c4a0b2421e56b153cb1c
ahaguilar <hiram1121@gmail.com>
2020-03-27 21:48:55 -0600

    Tweaks and changes, added dim light

---
+++ project/p2_interrupt_handler.c
@@ -0,0 +1,13 @@
+#include <msp430.h>
+#include "switches.h"
+#include "buzzer.h"
+#include "state_machine.h"
+
+/* Switch on P2 (S1) */
+
+void __interrupt_vec(PORT2_VECTOR) Port_2(){
+  if (P2IFG & SWITCHES) {            /* did a button cause this interrupt? */
+    P2IFG &= ~SWITCHES;            /* clear pending sw interrupts */
+    state_advance();                    /* single handler for all switches */
+  }
+}
